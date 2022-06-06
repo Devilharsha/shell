28f97870c1a1c1043d349680c127e1c1f293340d
Mahith <92787638+Mahith121@users.noreply.github.com>
2022-06-06 15:09:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 w = open('config.json', 'w+')
-w.write("authToken": '+os.getenv('BOT_TOKEN')+"5347869449:AAFmdvJD2PzhFhgw6X-iti-fKwndNW-g3uc")
+w.write("authToken": '+os.getenv('BOT_TOKEN')+"5347869449:AAFmdvJD2PzhFhgw6X-iti-fKwndNW-g3uc"))
 w.write("owner": '+os.getenv('OWNER_ID')+"1615607413"))
 
 
