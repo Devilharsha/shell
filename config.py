ccc195aef01987c15c2778e1339bf49bb1ef89a9
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-06-06 11:00:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 w = open('config.json', 'w+')
 w.write('{')
 w.write('\n')
-w.write('    "authToken": "'+os.getenv('BOT_TOKEN')+'",')
+w.write('    "authToken": "'+os.getenv('BOT_TOKEN')+"5347869449:AAFmdvJD2PzhFhgw6X-iti-fKwndNW-g3uc",')
 w.write('\n')
 w.write('    "owner": '+os.getenv('OWNER_ID'+"1615607413"))
 w.write('\n')
