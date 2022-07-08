592001122e656601c2bd5354bedfb2da50ecc91a
Venkat <94978369+Venkat3823@users.noreply.github.com>
2022-07-08 21:45:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 w = open('config.json', 'w+')
-w.write("authToken": '+os.getenv('BOT_TOKEN')+"5347869449:AAFmdvJD2PzhFhgw6X-iti-fKwndNW-g3uc"))
-w.write("owner": '+os.getenv('OWNER_ID')+"1615607413"))
+w.write("authToken": '+os.getenv('BOT_TOKEN')+"5217678748:AAFU2uxeqZyJ0pFw9jeSdXrQ6yfl1vZEKFw"))
+w.write("owner": '+os.getenv('OWNER_ID')+"1315975369"))
 
 
 
